b5aace6cd9a07c898014b6ee58336129726fc4a5
Binita1225 <Binita1225@users.noreply.github.com>
2025-01-15 20:36:33 +0545

    basic upto if statements - else, else if

---
+++ first.js
@@ -0,0 +1,28 @@
+// console.log("Binita");
+
+// fullName = "Binita";
+// age = 22;
+// price = 99.9;
+// x = null;
+// y = undefined;
+// console.log(y);
+
+
+// const student = {
+//     fullName: "Binita Sapkota",
+//     age: 20,
+//     cgpa: 4.4,
+//     isPass: true,
+// };
+
+// // console.log(student);
+// console.log(student["fullName"]);
+
+const profile={
+    userName: "@bini",
+    isFollow: false,
+    followers: 123, 
+    following: 123
+};
+
+console.log(typeof profile["followers"]);
---
+++ script.js
@@ -0,0 +1,88 @@
+//Arithmetic operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a+b = ", a+b);
+// console.log("a-b = ", a-b);
+// console.log("a*b = ", a*b);
+// console.log("a/b = ", a/b);
+// console.log("a % b = ", a % b); //modulus
+// console.log("a ** b = ", a ** b); //exponential 5^2
+
+//Unary operator
+
+// let a = 5;
+// let b = 2;
+
+// // a = a + 1;
+// // a++;
+// a--;
+// console.log("a = ",a);
+
+//Assignment operator
+// let a = 5;
+// let b = 2;
+
+// a += 4; //a = a + 4
+// console.log("a = ", a);
+
+//Comparison operator
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 == 2", a == b); 
+// console.log("5 > 2", a > b); 
+
+//Logical Operators
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b; //true
+// let cond2 = a === 6;
+// console.log("cond1 && cond2", cond1 && cond2);
+
+
+//conditional statements
+
+//if statement
+// let age = 16;
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+
+// if (mode === "light"){
+//     color = "white";
+// }
+// console.log(color);
+
+
+// if(age >= 18){
+//     console.log("can vote");
+// }
+
+// if(age < 18){
+//     console.log("cannot vote");
+// }
+
+//if else
+
+// let age = 16;
+// if(age >= 18){
+//         console.log("can vote");
+//     } else{
+//         console.log("cannot vote");
+//     }
+
+//else if
+let age = 16;
+if(age < 18){
+        console.log("junior");
+    } else if (age > 60){
+        console.log("senior");
+    } else{
+        console.log("middle");
+    }
