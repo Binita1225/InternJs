529363d12b355c0c696b77c4e3b757f8fca97810
Binita1225 <Binita1225@users.noreply.github.com>
2025-01-17 16:03:45 +0545

    upto query selector

---
+++ scriptDOM.js
@@ -0,0 +1,15 @@
+// let heading = document.getElementById("heading1");
+// console.dir(heading);
+
+// let headings = document.getElementsByClassName("heading2");
+// console.dir(headings);
+// console.log(headings);
+
+// let parahs = document.getElementsByTagName("p");
+// console.dir(parahs);
+
+// let elements = document.querySelector("p");
+// console.dir(elements);
+
+let elementAll = document.querySelectorAll(".heading2");
+console.dir(elementAll);
