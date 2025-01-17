56a39d05ece53fb7951b4f849690299ed6f580e8
Binita1225 <Binita1225@users.noreply.github.com>
2025-01-17 12:01:54 +0545

    array methods

--- script.js
+++ script.js
@@ -318,3 +318,120 @@
 // console.log(val);
 
 //sum function
+// function sum(a, b){
+//     return a + b;
+// }
+
+// const arrowSum = (a, b) =>{
+//     return a + b;
+// };
+
+// let arrowMul = (a, b) => {
+// return a * b;
+// }
+
+//  const printHello = () => console.log("hello");
+
+// //practice vowel
+// function countVowels(str){
+//     let count = 0;
+//     for (const char of str){
+//         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//             count++;
+//         }
+//     }
+//     // console.log(count);
+//     return count;
+// }
+
+// //arrow
+
+//  const countVow = (str) =>{
+//     let count = 0;
+//     for (const char of str){
+//         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//             count++;
+//         }
+//     }
+//     // console.log(count);
+//     return count;
+//  }
+
+// let arr = ["abc", "bcd", "cde"];
+
+// // arr.forEach(function printVal(val){
+// //     console.log(val);
+// // }); 
+
+// arr.forEach((val, idx, arr) =>{
+//     console.log(val.toUpperCase(), idx, arr);
+// });
+
+//practice arr square 
+
+// let nums = [1, 2, 3, 4];
+
+// // nums.forEach((num) => {
+// //     console.log(num * num);
+// // });
+
+// let newArr = nums.map((val) => {
+//     return val * 2;
+// });
+
+// console.log(newArr);
+
+// let calcSquare = (num) => {
+//     console.log(num * num);
+// };
+
+// nums.forEach(calcSquare);
+
+//filter
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let evenArr = arr.filter((val) =>{
+//     return val % 2 === 0;
+// });
+// console.log(evenArr);
+
+//reduce
+// let arr = [1, 2, 3, 4];
+// const output = arr.reduce((res, curr) => {
+//     return res + curr;
+// });
+
+// let arr = [1, 2, 10, 3, 4];
+// const output = arr.reduce((prev, curr) => {
+//     return prev > curr ? prev :curr;
+// });
+
+// console.log(output);
+
+//practice
+// let marks = [97, 64, 32, 49, 99, 86];
+
+// let toppers = marks.filter((val) => {
+//     return val > 90;
+// });
+
+// console.log(toppers);
+
+let n = prompt("enter a number: ");
+let arr = [];
+
+for(let i=1; i<=n; i++){
+    arr[i-1] = i;
+}
+
+console.log(arr);
+
+let sum = arr.reduce((res, curr) => {
+    return res + curr;
+});
+
+console.log("sum =", sum);
+
+let factorial = arr.reduce((res, curr) => {
+    return res * curr;
+});
+console.log("factorial =", factorial);
